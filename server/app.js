f4a1a8017d1d52a5d5eca8a2834bffd95482019b
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-05-07 09:44:47 +0500

    setting up the server side

--- server/app.js
+++ server/app.js
@@ -1,8 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import morgan from "morgan";
 import connectDB from "./db/connect.js";
-dotenv.config();
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import auth from "./middleware/auth.js";
+import authRoutes from "./routes/authRoutes.js";
 
+const app = express();
+app.use(express.json());
 
+// routes
+app.use("/api/v1/auth", authRoutes);
 
+// middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.port || 4000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+      console.log(`server listening on the ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
