0bee8c1efae60f32cd2c5fd5cc06a7e96778e215
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-05-09 08:43:48 +0500

    smll fix

--- server/models/User.js
+++ server/models/User.js
@@ -81,7 +81,7 @@ const UserSchema = new mongoose.Schema(
 
 UserSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(process.env.SALT_ROUNDS || 10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
