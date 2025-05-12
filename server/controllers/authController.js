866e03f04f9b938e5c096e4494b406bbc21af872
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-05-12 11:59:16 +0500

    working on the login module

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,9 @@
 import { StatusCodes } from "http-status-codes";
-import { attachCookiesToResponse, createTokenUser,sendVerificationEmail } from '../utils/index.js';
+import {
+  attachCookiesToResponse,
+  createTokenUser,
+  sendVerificationEmail,
+} from "../utils/index.js";
 import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
 import crypto from "crypto";
 import User from "../models/User.js";
@@ -17,9 +21,17 @@ const register = async (req, res, next) => {
     companySize,
     industry,
   } = req.body;
-  
 
-  if (!name || !email || !password || !lastName || !location?.country || !location?.city || !role || !phone) {
+  if (
+    !name ||
+    !email ||
+    !password ||
+    !lastName ||
+    !location?.country ||
+    !location?.city ||
+    !role ||
+    !phone
+  ) {
     throw new BadRequestError("Please provide all the values");
   }
   if (role === "employer") {
@@ -83,7 +95,6 @@ const register = async (req, res, next) => {
   });
 };
 
-
 const login = async (req, res, next) => {
   const { email, password } = req.body;
 
@@ -105,30 +116,30 @@ const login = async (req, res, next) => {
   }
   const tokenUser = createTokenUser(user);
   //refresh token
-  let refreshToken = '';
+  let refreshToken = "";
   const existingToken = await Token.findOne({ user: user._id });
-  if(existingToken){
+  if (existingToken) {
     const { isValid } = existingToken;
     if (!isValid) {
-      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+      throw new UnAuthenticatedError("Invalid Credentials");
     }
-    refreshToken=existingToken.refreshToken
+    refreshToken = existingToken.refreshToken;
     attachCookiesToResponse({ res, user: tokenUser, refreshToken });
   }
-  refreshToken= crypto.randomBytes(40).toString('hex');
-  const userAgent = req.headers['user-agent'];
+  refreshToken = crypto.randomBytes(40).toString("hex");
+  const userAgent = req.headers["user-agent"];
   const ip = req.ip;
-  const userToken= {refreshToken,ip,userAgent,user:user._id};
-  await Token.create(userToken)
-  attachCookiesToResponse({ res, user: tokenUser,refreshToken });
+  const userToken = { refreshToken, ip, userAgent, user: user._id };
+  await Token.create(userToken);
+  attachCookiesToResponse({ res, user: tokenUser, refreshToken });
 
-  res.status(StatusCodes.OK).json({tokenUser })
+  res.status(StatusCodes.OK).json({ tokenUser });
 };
 
 const updateUser = async (req, res, next) => {
   const { email, name } = req.body;
   if (!email || !name) {
-    throw new CustomError.BadRequestError('Please provide all values');
+    throw new BadRequestError("Please provide all values");
   }
   const user = await User.findOne({ _id: req.user.userId });
 
@@ -141,17 +152,76 @@ const updateUser = async (req, res, next) => {
   attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
-const showCurrentUser = async (req, res) => {
-  console.log(req);
-  
-  res.status(StatusCodes.OK).json({ user: req.user });
-};
-const logout = async (req, res) => {
-  res.cookie('token', 'logout', {
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000),
-  });
-  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
+
+const updateUserPassword = async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword) {
+    throw new UnAuthenticatedError("Please provide both values");
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError("Invalid Credentials");
+  }
+
+  user.password = newPassword;
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Success! Password Updated." });
 };
 
-export { register, login, updateUser,logout,showCurrentUser };
+const showCurrentUser = async (req, res) => {
+  res.status(StatusCodes.OK).json({ user: req.user });
+};
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnAuthenticatedError("Please provide valid email address");
+  }
+  if (verificationToken !== user.verificationToken) {
+    throw new UnAuthenticatedError("Verification Failed");
+  }
+  (user.isVerified = true),
+    (user.verified = Date.now()),
+    (user.verificationToken = "");
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: "Email Verified" });
+};
+
+const logout = async (req, res) => {
+  console.log({
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+    ip: req.ip,
+  });
+  res.cookie("accessToken", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000),
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+  });
+
+  res.cookie("refreshToken", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000),
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
+};
+
+export {
+  register,
+  login,
+  updateUser,
+  logout,
+  showCurrentUser,
+  verifyEmail,
+  updateUserPassword,
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import { register, login, updateUser,logout,showCurrentUser } from "../controllers/authController.js";
+import { register, login, updateUser,logout,showCurrentUser,verifyEmail,updateUserPassword } from "../controllers/authController.js";
 
 import authenticateUser from "../middlewares/auth.js";
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
+router.route("/verify-Email").get(verifyEmail);
 router.route("/updateUser").patch(authenticateUser, updateUser);
 router.route("/showCurrentUser").get(authenticateUser,showCurrentUser);
+router.route("/showCurrentUser").patch(authenticateUser,updateUserPassword);
 
 export default router;
