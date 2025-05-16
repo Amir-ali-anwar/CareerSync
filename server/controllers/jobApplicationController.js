0f01450ae170c07ad1f4c335608df877970637fd
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-05-16 06:44:07 +0500

    working on the Job module

--- server/controllers/jobApplicationController.js
+++ server/controllers/jobApplicationController.js
@@ -1,7 +1,7 @@
 import JobModal from "../models/JobsModal.js";
 import JobApplicationModal from "../models/JobApplicationModal.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError } from "../errors/index.js";
+import { BadRequestError,NotFoundError } from "../errors/index.js";
 import { checkPermissions } from "../middlewares/permissions.js";
 
 export const getJobApplications = async (req, res) => {
@@ -11,9 +11,9 @@ export const getJobApplications = async (req, res) => {
     .populate({
       path: "job",
       populate: {
-        path: "createdBy", // populate the job creator (employer)
+        path: "createdBy", 
         model: "User",
-        select: "name email role", // optional: only return needed fields
+        select: "name email role",
       },
     });
   if (!jobApplicants || jobApplicants.length === 0) {
@@ -25,11 +25,17 @@ const job = jobApplicants[0].job;
     return res.status(404).json({ error: "Job or creator info not found." });
   }
 
-  // ✅ Use your custom permission check here
   checkPermissions(req.user, job.createdBy._id);
   res.status(StatusCodes.OK).json({ msg: jobApplicants });
 };
 
-export const applicationStatus = async (req, res) => {
+export const updateApplicationStatus = async (req, res) => {
   const { jobId, applicantId } = req.params;
+  const { status } = req.body;
+  if (!["pending", "shortlisted", "rejected", "hired"].includes(status)) {
+    throw new BadRequestError("Invalid status");
+  }
+  const job = JobApplicationModal.findById({ job: jobId });
+  console.log({ job });
+  if (!job) throw new NotFoundError("Job not found");
 };
--- server/routes/jobApplicationRoutes.js
+++ server/routes/jobApplicationRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { getJobApplications } from "../controllers/jobApplicationController.js";
+import { getJobApplications,updateApplicationStatus } from "../controllers/jobApplicationController.js";
 import { authorizePermissions } from "../middlewares/permissions.js";
 
 router
   .route("/job/:jobId")
   .get(authorizePermissions("employer"), getJobApplications);
+
+router
+  .route("/applications/:jobId/:applicantId/status")
+  .patch(authorizePermissions("employer"), updateApplicationStatus);
+
 
 export default router;
