2c76f0e0409cf606e80972af4ac820f7a8e5f1d8
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-05-19 13:16:57 +0500

    organization module started

---
+++ server/controllers/organizationController.js
@@ -0,0 +1,10 @@
+import OrganizationModal from '../models/OrganizationModal.js'
+import { StatusCodes } from "http-status-codes";
+import {BadRequestError} from "../errors/index.js";
+import {checkPermissions} from "../middlewares/permissions.js";
+
+export const createOrganization= async(req,res)=>{
+
+}
+
+
