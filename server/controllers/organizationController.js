67cb69fc1238fb965a4cd7d1fccaa08751b1459a
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-05-19 13:53:24 +0500

    Organization module

--- server/controllers/organizationController.js
+++ server/controllers/organizationController.js
@@ -4,6 +4,19 @@ import {BadRequestError} from "../errors/index.js";
 import {checkPermissions} from "../middlewares/permissions.js";
 
 export const createOrganization= async(req,res)=>{
+const {name,description,industry,companySize,headquarters,about}= req.body
+if (
+  !name?.trim() ||
+  !description?.trim() ||
+  !industry?.trim() ||
+  !companySize?.trim() ||
+  !headquarters?.city?.trim() ||
+  !headquarters?.country?.trim() ||
+  !about?.trim()
+ 
+) {
+  throw new Error("All required organization fields must be provided");
+}
 
 }
 
