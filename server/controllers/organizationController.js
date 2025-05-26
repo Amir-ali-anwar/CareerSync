f2fa036c6dd4dab478a4222723e3a75929465e75
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-05-26 08:27:47 +0500

    update organization controller completed

--- server/controllers/organizationController.js
+++ server/controllers/organizationController.js
@@ -1,6 +1,6 @@
 import OrganizationModal from "../models/OrganizationModal.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError } from "../errors/index.js";
+import { BadRequestError,NotFoundError } from "../errors/index.js";
 import { checkPermissions } from "../middlewares/permissions.js";
 import validator from "validator";
 
@@ -42,18 +42,20 @@ export const createOrganization = async (req, res) => {
     !hiringContactEmail?.trim() ||
     !emailDomain?.trim()
   ) {
-    throw new Error("All required organization fields must be provided");
+    throw new BadRequestError(
+      "All required organization fields must be provided"
+    );
   }
 
   if (website && !validator.isURL(website)) {
-    throw new Error("Invalid website URL");
+    throw new BadRequestError("Invalid website URL");
   }
   if (socialLinks) {
     if (socialLinks.linkedin && !validator.isURL(socialLinks.linkedin)) {
-      throw new Error("Invalid LinkedIn URL");
+      throw new BadRequestError("Invalid LinkedIn URL");
     }
     if (socialLinks.twitter && !validator.isURL(socialLinks.twitter)) {
-      throw new Error("Invalid Twitter URL");
+      throw new BadRequestError("Invalid Twitter URL");
     }
   }
 
@@ -104,5 +106,34 @@ export const getAllOrganizations = async (req, res) => {
     createdBy: req.user.userId,
   });
   checkPermissions(req.user, organizationListing[0].createdBy.toString());
-  return res.status(StatusCodes.OK).json({ organizationListing, OrganizationCount:organizationListing.length });
+  return res
+    .status(StatusCodes.OK)
+    .json({
+      organizationListing,
+      OrganizationCount: organizationListing.length,
+    });
+};
+
+export const updateOrganization = async (req, res) => {
+  const { id: organizationId } = req.params;
+  const updateData = req.body;
+  const currentOrganization = await OrganizationModal.findOne({
+    _id: organizationId,
+  });
+  if (!currentOrganization) {
+    throw new NotFoundError("Organization not found");
+  }
+  checkPermissions(req.user, currentOrganization.createdBy.toString());
+  delete req.body.createdBy;
+  const updatedOrganization = await OrganizationModal.findByIdAndUpdate(
+    organizationId,
+    updateData,
+    { new: true, runValidators: true }
+  );
+  return res
+    .status(StatusCodes.OK)
+    .json({
+      msg: "Organization updated successfully",
+      organization: updatedOrganization,
+    });
 };
--- server/routes/OrganizationRoutes.js
+++ server/routes/OrganizationRoutes.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { authorizePermissions } from "../middlewares/permissions.js";
-import uploadCV from "../middlewares/fileuploader.js";
 const router = Router();
-
-import { createOrganization,getAllOrganizations } from "../controllers/organizationController.js";
-
-
+import { createOrganization,getAllOrganizations,updateOrganization } from "../controllers/organizationController.js";
 router
   .route("/")
   .post(authorizePermissions('employer'), createOrganization)
-
 router
   .route("/")
   .get(authorizePermissions('employer'), getAllOrganizations)
+
+  router
+    .route("/:id")
+    // .get(authorizePermissions('employer'), getJob)
+    .patch(authorizePermissions('employer'), updateOrganization)
+    // .delete(authorizePermissions('employer'), deleteJob);
 
 export default router;
